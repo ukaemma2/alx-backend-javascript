c78890d17816cd77a5d5c22fe5c8382a9a3dd364
ukaemma2 <ukaemmanuel2000@gmail.com>
2024-05-29 15:07:56 +0000

    0-promise.js

---
+++ 0x01-ES6_promise/0-main.js
@@ -0,0 +1,6 @@
+0-main.js
+import getResponseFromAPI from "./0-promise.js";
+
+const response = getResponseFromAPI();
+console.log(response instanceof Promise);
+
---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-constant-condition */
+export default function getResponseFromApi() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
