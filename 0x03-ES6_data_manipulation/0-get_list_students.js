9d491a07f65b2372ff041968cc3ddd40b15a7e36
ukaemma2 <ukaemmanuel2000@gmail.com>
2024-06-04 20:13:56 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { firstName: 'Guillanume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
